e2e9382d5a4d6bb1e1d79c00d94fde832306afee
Amazing <thienphuoc.0108@gmail.com>
2019-04-08 15:24:05 -0400

    James 1st commit

--- delete_item.php
+++ delete_item.php
@@ -18,7 +18,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     if(empty(trim($_POST["product_name"]))){
         $product_name_err = "Please enter the product name.";
     } else{
-        $product_name = $_POST["product_name"];
+        $product_name = trim($_POST["product_name"]);
     }
 
     //Delete the data in the product table
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group" <?php echo (!empty($product_name_err)) ? 'has-error' : ''; ?>>
             <label>Product name</label>
-            <input type="text" name="product_name" class="form-control">
+            <input type="text" name="product_name" class="form-control" />
             <span class="help-block"><?php echo $product_name_err; ?></span>
         </div>
         <div class="form-group">
